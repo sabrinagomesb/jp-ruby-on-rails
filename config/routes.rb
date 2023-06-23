75dd6b7421d48c9b5ff504df1d8acb2cc7eaa266
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-06-23 20:37:40 -0300

    feat: gera controller e view 'welcome'

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'welcome/index'
   resources :coins
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
