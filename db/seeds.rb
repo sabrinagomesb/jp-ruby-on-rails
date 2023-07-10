c3bf7d02bb36f3db792150ec703a3dc1c0a9c08a
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-07-10 12:12:04 -0300

    adiciona seeds básico para testar aplicacao

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,3 +7,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cadastrando moedas..."
+
+Coin.create!(description: 'Bitcoin', acronym: 'BTC', url_image: 'https://logodownload.org/wp-content/uploads/2017/06/bitcoin-logo-1-1.png')
+
+Coin.create!(description: 'Ethereum', acronym: 'ETH', url_image: 'https://cryptologos.cc/logos/ethereum-eth-logo.png')
+
+Coin.create!(description: 'DogCoin', acronym: 'DOGE', url_image: 'https://upload.wikimedia.org/wikipedia/pt/d/d0/Dogecoin_Logo.png')
+
+puts "Moedas cadastradas com sucesso."
