54a039b8d6eb94a4d638e86231b8a7ae6e551343
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-08-03 20:00:45 -0300

    feat: adiciona libs javascript jquery e notify

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,7 @@ pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
 pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 pin_all_from 'app/javascript/controllers', under: 'controllers'
+
+pin 'jquery', to: 'jquery.js', preload: true
+pin 'notify', to: 'notify.js', preload: true
+
